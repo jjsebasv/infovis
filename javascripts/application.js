dd3022adf90cdeda554d2f332e90e14c592aae26
jjsebasv <sebastian.vera@colegiochampagnat.com.ar>
2016-03-11 00:31:13 -0300

    reader

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,31 +1,9 @@
 $( document ).ready(function() {
 
-  var dataset = [
-    { word: "hola",
-      quantity: 50
-    },
-    { word: "chau",
-      quantity: 25
-    },
-    { word: "gilada",
-      quantity: 73
-    },
-    { word: "cabida",
-      quantity: 28
-    },
-    { word: "antes",
-      quantity: 9
-    },
-    { word: "kevin",
-      quantity: 1
-    },
-    { word: "visualizacion",
-      quantity: 19
-    },
-   ];
+  var dataset = [{"word":"graphical","quantity":6},{"word":"user","quantity":3},{"word":"large","quantity":3},{"word":"items","quantity":3},{"word":"interactive","quantity":4},{"word":"visual","quantity":9},{"word":"representations","quantity":6},{"word":"abstract","quantity":11},{"word":"data","quantity":34},{"word":"amplify","quantity":4},{"word":"cognition","quantity":5},{"word":"card","quantity":7},{"word":"information","quantity":29},{"word":"visualization","quantity":28},{"word":"computer","quantity":7},{"word":"graphics","quantity":4},{"word":"interaction","quantity":4},{"word":"2008","quantity":4},{"word":"that","quantity":6},{"word":"human","quantity":3},{"word":"with","quantity":6},{"word":"visualizations","quantity":3},{"word":"2005","quantity":3},{"word":"important","quantity":4},{"word":"charts","quantity":3},{"word":"such","quantity":3},{"word":"techniques","quantity":4},{"word":"scientific","quantity":3},{"word":"typically","quantity":3},{"word":"1998","quantity":4},{"word":"science","quantity":3},{"word":"this","quantity":8},{"word":"space","quantity":8},{"word":"more","quantity":3},{"word":"sometimes","quantity":3},{"word":"called","quantity":4},{"word":"kind","quantity":3},{"word":"which","quantity":3},{"word":"image","quantity":3},{"word":"screen","quantity":3},{"word":"makes","quantity":3},{"word":"examples","quantity":3},{"word":"attributes","quantity":3},{"word":"coordinates","quantity":3},{"word":"unstructured","quantity":3},{"word":"records","quantity":5},{"word":"different","quantity":3},{"word":"criteria","quantity":4},{"word":"each","quantity":3}];
 
-  var sizeScale = d3.scale.linear().domain([1,73]).range([8,40]);
-  var colorScale = d3.scale.linear().domain([1,73]).range(["blue", "red"]);
+  var sizeScale = d3.scale.linear().domain([3,34]).range([12,60]);
+  var colorScale = d3.scale.linear().domain([3,34]).range(["royalBlue", "tomato"]);
   //var colorScale = d3.scale.category10();
 
   function mySort(dataset) {
--- utilities/word_reader.rb
+++ utilities/word_reader.rb
@@ -11,12 +11,37 @@ file.each_line do |line|
   array_line = line.split(' ')
   array_line.each do |word|
     sanitized_word = sanitize_word(word)
-    if ans.key?(sanitized_word)
-      ans[sanitized_word] += 1
-    else
-      ans[sanitized_word] = 1
+    if sanitized_word.length > 3
+      if ans.key?(sanitized_word)
+        ans[sanitized_word] += 1
+      else
+        ans[sanitized_word] = 1
+      end
     end
   end
 end
 
-File.open('./result.json', 'w+').write(JSON.generate(ans.to_a))
+h_ans = []
+min = 100
+max = 0
+
+ans.each do |key, value|
+  if value > 2
+    if value < min
+      min = value
+    end
+    if value > max
+      max = value
+    end
+    aux = {
+      word: key,
+      quantity: value
+    }
+    h_ans << aux
+  end
+end
+
+puts min
+puts max
+
+File.open('./result.js', 'w+').write(JSON.generate(h_ans))
