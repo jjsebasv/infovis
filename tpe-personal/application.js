33f223f1486df403d8dc4e15c17abb4447aa1ff1
Juan Sebastian Vera <jj.sebas.v@gmail.com>
2016-04-25 16:16:36 -0300

    json

--- tpe-personal/application.js
+++ tpe-personal/application.js
@@ -27,13 +27,39 @@ $( document ).ready(function() {
   });
   var sortable = [];
   for (var data in dataset)
-        sortable.push([data, dataset[data]])
+    sortable.push([data, dataset[data]])
   sortable.sort(function(a, b) {return b[1].amount - a[1].amount})
 
-  console.log(dataset);
-  var url = 'data:text/json;charset=utf8,' + encodeURIComponent(JSON.stringify(dataset));
-  localStorage.setItem('shape', JSON.stringify(dataset))
-  window.open(url, '_blank');
-  window.focus();
+  var getJson = function(my_dataset) {
+    var url = 'data:text/json;charset=utf8,' + encodeURIComponent(JSON.stringify(my_dataset));
+    localStorage.setItem('shape', JSON.stringify(my_dataset))
+    window.open(url, '_blank');
+    window.focus();
+  };
+
+  var relevancy = {};
+
+  d3.json("file:///home/jvera/Escritorio/Visualizacion/tpe-personal/data.json", function(json) {
+    console.log(json);
+  });
+
+  for (var data in dataset) {
+    relevancy[data] = {
+      "relevant": 0,
+      "irrelevant": 0,
+      "total": 0
+    };
+    $.each(dataset[data].messages, function(){
+      if ($(this).relevant) {
+        relevancy[data].relevant ++;
+      } else {
+        relevancy[data].irrelevant ++;
+      };
+      relevancy[data].total ++;
+    });
+  };
+  console.log(relevancy);
+
+  getJson(dataset);
 
 });
